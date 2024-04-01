e7355064b9305ad3a5bd09b04a5b4cc3c161a2c5
victoriafpizza <rm550609@fiap.com.br>
2024-04-01 09:34:59 -0300

    ADCIONANDO DADOS EM FOR EM LOOP

---
+++ 1semestre/aula8-for/tutorial.java
@@ -0,0 +1,9 @@
+
+
+for (int x = 0; x < y; x++) {
+    System.out.print("LERO LEROO")...
+}
+
+//for (inicialização da variável; checagem de condição; incremento/decremento do valor da variável) {  
+    //comando a ser executado/declaração
+//}
