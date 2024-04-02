33f100a7db26aefec84b8f8b6ef1951672e9616b
victoriafpizza <rm550609@fiap.com.br>
2024-04-02 08:59:11 -0300

    adcioando exercicios de matriz

---
+++ 1semestre/aula14-vetoresematrizes/matriz.java
@@ -0,0 +1,20 @@
+int valor=90;
+int [] valor = new int[10];
+int [] valor = {1, 4,};
+
+///matriz - declaração
+
+int [] [] valor = new int[2] [2];
+
+
+import javax
+// vetor
+
+for(int c=0 ; c < 2;  c++){
+    for(int c=0 ; c < 2;  c++){
+
+String x = JOptionPane.showInputDialog(null, "Digite o valor da linha" + l + "e da coluna" + c);
+valor[i][c] = Interger.parseInt(x);
+    }
+
+}
